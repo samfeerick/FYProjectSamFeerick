68ae812138b210ae6fc698edca8d58c88fcd9043
samfeerick <55976442+samfeerick@users.noreply.github.com>
2020-05-17 15:42:25 +0100

    Update lcd_16x2.py

--- RFIDPythonFiles/lcd_16x2.py
+++ RFIDPythonFiles/lcd_16x2.py
@@ -1,35 +1,5 @@
 #!/usr/bin/python
-#--------------------------------------
-#    ___  ___  _ ____
-#   / _ \/ _ \(_) __/__  __ __
-#  / , _/ ___/ /\ \/ _ \/ // /
-# /_/|_/_/  /_/___/ .__/\_, /
-#                /_/   /___/
-#
-#  lcd_16x2.py
-#  16x2 LCD Test Script
-#
-# Author : Matt Hawkins
-# Date   : 06/04/2015
-#
-# http://www.raspberrypi-spy.co.uk/
-#
-# Copyright 2015 Matt Hawkins
-#
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-#
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
-#--------------------------------------
+
 
 # The wiring for the LCD is as follows:
 # 1 : GND
